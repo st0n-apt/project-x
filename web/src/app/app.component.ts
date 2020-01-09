5d8feee436eafea92414b12a56a697ac76b4d0ae
s <jaime@cavero-sanchez.com>
2020-01-09 19:00:23 +0100

    Web added

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'], // hoja de estilos
+})
+export class AppComponent {
+  title = 'Music X';
+}
